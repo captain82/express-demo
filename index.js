602aa7674096f0e861034666621e6ade75c8f76d
ankitkr82 <ankit.kumar@rapido.bike>
2021-09-10 21:22:30 +0530

    Pug view engine added

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const startupDebugger = require('debug')('app:startup');
+const dbDebugger = require('debug')('app:db');
 const config= require('config');
 const Joi = require('joi');
 const morgan = require('morgan');
@@ -5,6 +7,9 @@ const helmet = require('helmet');
 const express = require('express');
 const logger = require('./logger');
 const app = express();
+
+app.set('view engine' , 'pug');
+app.set('views','./views');
 
 app.use(express.json());
 app.use(logger);
@@ -19,8 +24,15 @@ console.log('Mail server: '+config.get('mail.host'));
 
 if(app.get('env') === 'development'){
     app.use(morgan('tiny'));
-    console.log('Morgan enabled...');
+    startupDebugger('Morgan enabled...');
 }
+
+app.get('/' , (req,res)=>{
+    res.render('index',{ title:'My Express app',message:'Hello' });
+});
+
+//Db work
+dbDebugger('Conneccted to the database');
 
 const courses = [
     {id: 1,name : 'course1'},
